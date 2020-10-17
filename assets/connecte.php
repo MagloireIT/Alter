6820293424cb0b06535ffad8f105cfcc75bfe958
Magloire <magloiredjomolite1@gmail.com>
2020-10-17 22:36:57 +0200

    Version 0.2

---
+++ assets/connecte.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Alter</title>
+</head>
+<body>
+
+<?php include '../element/navbar.php'; ?>
+<br>
+<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-1">
+      </div>
+    <div class="col-md-11"> 
+    <h1>Bienvenue dans notre espace commun</h1>
+    </div>
+    </div>
+</div>  
+
+     <div class="container-fluid">
+         <div class="row">
+         <div class="col-md-1"> </div>
+             <div class="col-md-7 page">
+                 <br>
+             <h1 style="color:grey">A l'affiche</h1>    
+
+             <img src="../img/affiche1.jpg" class="img-fluid" alt="Image Affiche">
+            
+             <br>
+             <br>
+             <br>
+
+             <div class="d-flex flex-row">
+                  <div class="p-2"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ex porro repellat tempora? Doloru
+                      m possimus, nobis facilis voluptatibus reprehenderit veniam adipisci hic blandi
+                      tiis, cumque ullam fugiat, assumenda in officia amet. </p></div>
+
+
+
+                  <div class="p-2">  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ex porro repellat tempora? Doloru
+                      m possimus, nobis facilis voluptatibus reprehenderit veniam adipisci hic blandi
+                      tiis, cumque ullam fugiat, assumenda in officia amet. </p> </div>
+
+
+                  <div class="p-2"> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ex porro repellat tempora? Doloru
+                      m possimus, nobis facilis voluptatibus reprehenderit veniam adipisci hic blandi
+                      tiis, cumque ullam fugiat, assumenda in officia amet. </p></div>
+            </div>
+
+
+
+        
+                  
+            
+            
+            
+            
+            </div>
+
+             <div class="col-md-1"> <hr> </div>
+             <div class="col-md-2">
+                 
+                <aside> <h3>Film a commenter</h3>
+                <br>
+                <br>
+            <article>
+               <h4>Tenet</h4>
+               <img src="../img/tenet.jpg" class="img-fluid" alt="tenet">
+               <caption> Voici le film qui fait la une des cinéma </caption>
+
+            </article>
+            <br>
+            <br>
+
+            <article>
+               <h4>Antebellum</h4>
+               <img src="../img/Antebellum.png" class="img-fluid" alt="Antebellum">
+               <caption> Avis sur ce film ? </caption>
+
+            </article>
+
+
+
+        
+                </aside>
+            </div>
+            <div class="col-md-1"> </div>
+
+        </div>
+    </div>
+
+
+
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
---
+++ element/navbar.php
@@ -0,0 +1,27 @@
+
+<h1 id="alter">Alter</h1>
+
+<header>
+
+    <nav class="menu">
+        <ul>
+        <li> <a href="www.google.com">Accueil </a></li>
+        <li> <a href="www.google.com">Présentation</a></li>
+        <li> <a href="www.google.com">Sujets</a></li>
+        <li><a href="www.google.com">Communautés</a></li>
+        <li><a href="www.google.com">Don </a></li>
+        </ul>
+
+    </nav>
+
+</header>
+
+
+
+
+
+
+
+
+
+
